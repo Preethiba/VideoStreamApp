32a8d260fb106b4b6e9709ee717f514778fad0fe
Preethiba <44765593+Preethiba@users.noreply.github.com>
2021-04-04 15:54:50 +0530

    Add files via upload

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component{
+
+    render(){
+        return(
+            <div>Google API</div>
+        );
+    }
+}
+
+export default GoogleAuth;
