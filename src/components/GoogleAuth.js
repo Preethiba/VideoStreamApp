0f2108af6af92f14a61de907c313671682250429
Preethiba <ibpreethiba99@gmail.com>
2021-04-06 17:41:53 +0530

    2 files modified

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 class GoogleAuth extends React.Component{
 
+    ComponentDidMount(){
+      window.gapi.
+    }
+
     render(){
         return(
             <div>Google API</div>
