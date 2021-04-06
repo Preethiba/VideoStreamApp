9a320c66645a800d9f8f19e03ad0ff50ce3fd569
Preethiba <ibpreethiba99@gmail.com>
2021-04-06 18:09:39 +0530

    1 file modified

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "142932054783-f12lqmv71js47kppj0128t55kpngn5o1.apps.googleusercontent.com",
-        scope: "email"
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "142932054783-f12lqmv71js47kppj0128t55kpngn5o1.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
   render() {
