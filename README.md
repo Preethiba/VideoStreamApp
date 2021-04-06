3488837b9619c11af98a7ae19bab28b3177505ca
Preethiba <ibpreethiba99@gmail.com>
2021-04-06 17:58:31 +0530

    3 files modified, 1 added

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # react-dh4p5w
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-dh4p5w)
+
+
+https://videostream.stackblitz.io
--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "142932054783-f12lqmv71js47kppj0128t55kpngn5o1.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
   render() {
     return <div>Google API</div>;
   }
