4dbd6b434abcee2b271b5e2328966fa973351352
Preethiba <ibpreethiba99@gmail.com>
2021-04-09 10:37:41 +0530

    1 file modified

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,15 +2,26 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput(formProps) {
-    return <input {...formProps.input} />; //assigns form props to input tag and returns that to field component
+  renderInput({ input, label }) {
+    //destructing input from formProps
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />{" "}
+        {/* assigns form props to input tag and returns that to field component */}
+      </div>
+    );
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter description"
+        />
       </form>
     );
   }
