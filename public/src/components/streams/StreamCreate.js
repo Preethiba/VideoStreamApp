47671806e72a1b340198d72c66388ecb25193217
Preethiba <ibpreethiba99@gmail.com>
2021-04-10 17:45:11 +0530

    1 file modified

--- public/src/components/streams/StreamCreate.js
+++ public/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -21,9 +23,7 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    
-  }
+  onSubmit(formValues) {}
 
   render() {
     return (
