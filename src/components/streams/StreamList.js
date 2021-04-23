c72e992227b9adec374e648a9209346d67b29c40
Preethiba <ibpreethiba99@gmail.com>
2021-04-23 10:19:37 +0530

    1 file modified

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -34,7 +34,7 @@ class StreamList extends React.Component {
   renderList() {
     return this.props.streams.map(stream => {
       return (
-        <div className="item" key={stream.title}>
+        <div className="item" key={stream.id}>
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
