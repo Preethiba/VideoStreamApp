bd18a32eb3c512bacd984cb6afc05ae1660b8057
Preethiba <ibpreethiba99@gmail.com>
2021-04-23 09:31:37 +0530

    1 file modified

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,9 +7,14 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
-  renderAdmin() {
+  renderAdmin(stream) {
     if (stream.userId === this.props.currentUserId) {
-      return <div>Edit/Delete</div>;
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
     }
   }
 
@@ -17,12 +22,12 @@ class StreamList extends React.Component {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.title}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
-          {this.renderAdmin(stream)}
         </div>
       );
     });
